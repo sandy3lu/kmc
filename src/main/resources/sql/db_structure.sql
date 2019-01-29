4a6135c90c4179162023eafc8f0570f7a0f23038
lurong <sandy3lu@126.com>
2019-01-29 16:07:55 +0800

    config

--- src/main/resources/sql/db_structure.sql
+++ src/main/resources/sql/db_structure.sql
@@ -1,6 +1,11 @@
-CREATE TABLE `dict_constant`(
+CREATE TABLE `key`(
 `id` int NOT NULL AUTO_INCREMENT COMMENT '自增id（常量编码）' ,
-`value` varchar(64) not null COMMENT '常量名称',
-`parent_id` int default null COMMENT '父id',
+`user_id` varchar(64) not null COMMENT '申请创建key的用户id',
+`key_type` ENUM('SM2Priv','SM2Pub','SM4','RSA2048Priv','RSA2048Pub','AES128') not null COMMENT 'key的类型',
+`content` varchar(2048) not null COMMENT 'key的内容',
+`related_key` int default null COMMENT 'private key关联的public key',
+`signature` varchar(128) default null COMMENT 'key的签名值',
+`sign_key` int default null COMMENT '签名用的key',
+`create_time` datetime COMMENT '创建时间',
 PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='通用字典表';
+) ENGINE=InnoDB  AUTO_INCREMENT=22 DEFAULT CHARSET=utf8 COMMENT='key管理表';
